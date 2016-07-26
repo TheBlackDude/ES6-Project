b3a4a0b9a31958d9879ed11ad7a513ca263f8fbb
Osman Jalloh <omudu45@hotmail.com>
2016-07-26 19:55:29 +0000

    Configured the gulpfile.js with babel.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 const gulp = require('gulp');
-// Gulp dependencies go here
+const babel = require('gulp-babel');
 
 gulp.task('default', function() {
-	// Gulp tasks go here
+	// Node source
+	gulp.src("es6/**/*.js")
+	   .pipe(babel())
+	   .pipe(gulp.dest("dist"));
+	// Browser source
+	gulp.src("public/es6/**/*.js")
+	   .pipe(babel())
+	   .pipe(gulp.dest("public/dist"));
 });
